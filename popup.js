bf208a691998c89a0f0726c13e29261b0e0943d6
ddiyoung <wsw3998@sju.ac.kr>
2021-06-30 03:55:06 +0900

    css

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-const check = () =>{
+{const check = () =>{
     $("input[type='checkbox']").click(() => {
         chrome.tabs.executeScript({
             file: 'contentScript.js'
@@ -6,4 +6,36 @@ const check = () =>{
     })
 }
 
-check();
+    const IsUrlRight = () => {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            currentTab = tabs[0].url.replace(/(^\w+:|^)\/\//, '');
+            if (currentTab === 'blackboard.sejong.ac.kr/ultra/course') {
+                $('.no-error').html(`
+                <div class ="popup-controlattend-container">
+                    <h2>출석 확인하기</h2>
+                    <label class="switch">
+                        <input type="checkbox" id="control-lock">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+                `);
+            } else {
+                $('.wrongSite').html(`
+                    <div class = "popup-error-container">
+                        <p class = "extension-txt">블랙보드 코스 창에서 실행해주세요</p>
+                    </div>
+                `);
+            }
+        });
+    }
+    
+    const main = () => {
+        IsUrlRight() 
+        check();
+    }
+
+    main();
+
+    
+
+}
