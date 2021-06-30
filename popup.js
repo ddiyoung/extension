c631c2563401e4c371d7e08696b9e765f1b3b332
ddiyoung <wsw3998@sju.ac.kr>
2021-07-01 05:45:34 +0900

    CSS, html 코드 수정

--- popup.js
+++ popup.js
@@ -12,13 +12,20 @@
             currentTab = tabs[0].url.replace(/(^\w+:|^)\/\//, '');
             if (currentTab === 'blackboard.sejong.ac.kr/ultra/course') {
                 $('.no-error').html(`
+                <h2>출석 확인하기</h2>
                 <div class ="popup-controlattend-container">
-                    <h2>출석 확인하기</h2>
+                    <div>
+                        <p class = "extension-txt">모든 강의 출석 확인하기</p>
+                        <p class = "extension-txt">Check Attendance For All Courses</p>
+                    </div>
                     <label class="switch">
                         <input type="checkbox" id="control-lock">
                         <span class="slider round"></span>
                     </label>
                 </div>
+                <p>
+                    <button id="CheckAtd" class = "extension-btn" type="button"> 출석 확인 하기 </button>
+                </p>
                 `);
             } else {
                 $('.wrongSite').html(`
@@ -26,7 +33,6 @@
                         <p class = "extension-txt">블랙보드 코스 탭에서 실행해주세요.</p>
                         <br>
                         <p class = "extension-txt">Please run in the blackboard course tab.</p>
-                        <br>
                         <p>
                             <button id="Black-Course" class="extension-btn" type="button"> 블랙보드 코스 바로가기 </button>
                         </p>
