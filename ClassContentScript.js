a230b75e3d9dac6743f4a02b81696fa8cba6a2ab
ddiyoung <wsw3998@naver.com>
2021-09-23 16:28:06 +0900

    create SeperateContentName and Function of Separate

--- ClassContentScript.js
+++ ClassContentScript.js
@@ -1,7 +1,38 @@
 {
-    const NowDate = () => {
-        const offset = new Date();
-        
-        return (new Date(offset.getTime() - (offset.getTimezoneOffset() * 60000))).toISOString();
-    }   
+
+    class SeparateContentName{
+        constructor(contentname){
+            this.lecturename = "";
+            this.attendance = "";
+            this.deadline = "";
+            this.contentname = contentname;
+        }
+
+        Separate(){
+            const idxSlash = this.contentname.lastIndexOf('/');
+            this.lecturename = this.contentname.slice(0, idxSlash);
+            const duedate = this.contentname(idxSlash+1, this.contentname.length).split('~');
+            this.attendance = duedate[0].trim();
+            this.deadline = duedate[1].trim();
+        }
+    }
+
+    class LectureInfo{
+        constructor() {
+            this.id = "";
+            this.week = "";
+            this.lecturename = "";
+            this.attendance = "";
+            this.deadline = "";
+            this.pass = "";
+            this.content_id = "";
+        }
+    }
+
+    class Course {
+        constructor() {
+            this.name = "";
+            this.course_id = "";
+        }
+    }
 }
