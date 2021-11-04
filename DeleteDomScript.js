2496c18771bfd950356f06bca410eb356d344c9c
ddiyoung <wsw3998@sju.ac.kr>
2021-11-05 04:24:01 +0900

    Delete windowEvent(scroll)

--- DeleteDomScript.js
+++ DeleteDomScript.js
@@ -4,7 +4,6 @@
         $('#classBody').remove();
         const siteMenu = document.getElementById('side-menu');
         siteMenu.style.transform = 'translateY(0px)';
-        window.removeEventListener('scroll');
     }
 
     const main = () =>{
